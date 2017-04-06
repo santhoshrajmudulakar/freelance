5a3e300cc5428e5076c6a13a9c4b88dfe7b018c7
santhosh <santhosh@azrisolutions.com>
2017-04-06 13:54:43 +0530

    Initial Commitl

---
+++ js/ee.js
@@ -0,0 +1,37 @@
+﻿// Tamino Martinius - All rights reserved
+
+// Copyright © 2013 Tamino Martinius (http://zaku.eu)
+// Copyright © 2013 Particleslider.com (http://particleslider.com
+
+// Terms of usage: http://particleslider.com/legal/license
+
+var init = function(){
+  var isMobile = navigator.userAgent &&
+    navigator.userAgent.toLowerCase().indexOf('mobile') >= 0;
+  var isSmall = window.innerWidth < 1000;
+  
+  var ps = new ParticleSlider({
+    ptlGap: isMobile || isSmall ? 3 : 0,
+    ptlSize: isMobile || isSmall ? 3 : 1,
+    width: 1e9,
+    height: 1e9
+  });
+  ps.mouseForce=500;
+  ps.ptlGap=5
+  ps.ptlSize=2;
+  ps.init(true);
+  
+}
+var initParticleSlider = function(){
+  var psScript = document.createElement('script');
+  (psScript.addEventListener
+    ? psScript.addEventListener('load', init, false)
+    : psScript.onload = init);
+  psScript.src = 'http://particleslider.com/js/particleslider/current/particleslider.js';
+	psScript.setAttribute('type', 'text/javascript');
+  document.body.appendChild(psScript);
+}
+   
+(window.addEventListener
+  ? window.addEventListener('load', initParticleSlider, false)
+  : window.onload = initParticleSlider);
